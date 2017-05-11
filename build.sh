16a1bc8d2e6faf72cc9b9d75dc9ec575359aaa2b
rlin <rlin@thoughtworks.com>
2017-05-11 10:53:22 +0800

    [@boydfd] build jekyll with docker and export to _build dir.

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 containerName=blog
 imageName=blog
+workDir=blog
+builtDir='_build'
 build() {
+	sed s/{{builtDir}}/${builtDir}/g ./Dockerfile.template > Dockerfile
 	docker build -t $imageName .
 }
 
 run() {
 	docker rm -f $containerName
-    docker run --name $containerName -p 4000:8000 $imageName
+    docker run --name $containerName -v $(pwd)/${builtDir}:/${workDir}/${builtDir} -p 4000:8000 $imageName
 }
 
 buildRun() {
