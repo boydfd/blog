19a458b21f3cf56fa32bcb28b691c49988433153
rlin <rlin@thoughtworks.com>
2017-05-11 13:29:57 +0800

    [@boydfd] add nginx.

--- build.sh
+++ build.sh
@@ -1,33 +1,68 @@
-containerName=blog
-imageName=blog
-workDir=blog
-builtDir='_build'
-build() {
+
+buildSite() {
+	siteContainerName=blog
+	siteImage=blog
+	siteWorkDir=blog
+	builtDir='_build'
 	sed s/{{builtDir}}/${builtDir}/g ./Dockerfile.template > Dockerfile
-	docker build -t $imageName .
+	docker build -t $siteImage .
 	rm -f Dockerfile
+	docker rm -f $siteContainerName
+	docker run --name $siteContainerName -v $(pwd)/${builtDir}:/${siteWorkDir}/${builtDir} $siteImage
+	docker images | grep none | awk '{print $3}' | xargs docker rmi
+}
+
+buildNginx() {
+	nginxImage=blog-nginx
+	nginxContainerName=blog-nginx
+	cp Dockerfile.nginx Dockerfile
+	docker build -t $nginxImage .
+	rm -f Dockerfile
+	docker images | grep none | awk '{print $3}' | xargs docker rmi
+
 }
 
 run() {
-	docker rm -f $containerName
-    docker run --name $containerName -v $(pwd)/${builtDir}:/${workDir}/${builtDir} -p 4000:8000 $imageName
+	nginxImage=blog-nginx
+	nginxContainerName=blog-nginx
+	docker rm -f $nginxContainerName
+	docker run -d --name $nginxContainerName -v $(pwd)/certs:/etc/nginx/certification -p 80:80 -p 443:443 $nginxImage
+}
+
+siteRun() {
+	buildSite
+	run
 }
 
 buildRun() {
-	build
-	run
+	buildSite
+	buildNginx
+    	run
 }
 case $1 in
-	-b)
-		build
+	site)
+		buildSite
 		;;
-	-r)
-		run
+	nginx)
+		buildNginx
 		;;
+	build)
+		buildSite
+		buildNginx
+		;;
+	run)
+    		run
+    		;;
 	-br)
 		buildRun
 		;;
 	-rb)
 		buildRun
+		;;
+	-sr)
+		siteRun
+		;;
+	-rs)
+		siteRun
 		;;
 esac
