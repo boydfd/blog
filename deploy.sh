8de938b5f519cf5d34fe7273e20ec70f6dfaba30
rlin <rlin@thoughtworks.com>
2017-05-15 15:55:20 +0800

    [@rlin] deploy after rebuild.

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 ssh rlin@35.187.205.160 'cd blog; git pull -r'
--- rebuild.sh
+++ rebuild.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
 ssh rlin@35.187.205.160 'cd blog; git pull -r;sudo bash ./build.sh site'
+docker-compose up
