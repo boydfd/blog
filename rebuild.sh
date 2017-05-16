e6060fa674d05ca0f36323258c86cd358c93da06
boydfd <boydfd@163.com>
2017-05-16 22:58:54 +0800

    [@boydfd] build nginx before deploy.

--- rebuild.sh
+++ rebuild.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ssh rlin@aboydfd.com 'cd blog; git pull -r;sudo bash ./build.sh site;sudo docker-compose up -d'
+ssh rlin@aboydfd.com 'cd blog; git pull -r;sudo docker-compose build;sudo docker-compose up -d'
