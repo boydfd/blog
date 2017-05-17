3d5bbfba0fc7ef3c253e690367d7e5fc7495f346
boydfd <boydfd@163.com>
2017-05-17 12:56:46 +0800

    [@boydfd] check git commit version.

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,10 @@ rebuild() {
 }
 update() {
 	echo 1
-	echo $GO_FROM_REVISION_FOO
-	echo $GO_TO_REVISION_FOO
+	echo $GO_TO_REVISION_BLOG
+	echo $GO_FROM_REVISION_BLOG
+	echo $GO_FROM_REVISION_GOCD
+	echo $GO_TO_REVISION_GOCD
 	echo 2
 	sshRemote 'cd blog; git pull -r'
 }
