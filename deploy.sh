409a8e7b9a154311357f85896e2e11e856166747
boydfd <boydfd@163.com>
2017-05-17 12:46:18 +0800

    [@boydfd] check git commit version.

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,21 @@
 #!/usr/bin/env bash
-ssh rlin@aboydfd.com 'cd blog; git pull -r'
+
+sshRemote() {
+	ssh rlin@aboydfd.com "$1"
+}
+rebuild() {
+	echo $GO_FROM_REVISION_FOO
+	ehco $GO_TO_REVISION_FOO
+	sshRemote 'cd blog; git pull -r;sudo docker-compose build;sudo docker-compose up -d'
+}
+update() {
+	sshRemote 'cd blog; git pull -r'
+}
+case $1 in
+	rebuild)
+		rebuild
+		;;
+	update)
+		update
+		;;
+esac
