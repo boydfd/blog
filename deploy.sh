2960d8bff95f26a602483903757455b781227356
boydfd <boydfd@163.com>
2017-05-17 12:53:24 +0800

    [@boydfd] check git commit version.

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,13 @@ sshRemote() {
 	ssh rlin@aboydfd.com "$1"
 }
 rebuild() {
-	set x
+	sshRemote 'cd blog; git pull -r;sudo docker-compose build;sudo docker-compose up -d'
+}
+update() {
 	echo 1
 	echo $GO_FROM_REVISION_FOO
 	echo $GO_TO_REVISION_FOO
 	echo 2
-	sshRemote 'cd blog; git pull -r;sudo docker-compose build;sudo docker-compose up -d'
-}
-update() {
 	sshRemote 'cd blog; git pull -r'
 }
 case $1 in
