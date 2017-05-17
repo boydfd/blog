3d76938604773e9e03ada931ea267d669eadd501
boydfd <boydfd@163.com>
2017-05-17 14:23:21 +0800

    [@boydfd] check changed file to decide which part to deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,66 @@
 #!/usr/bin/env bash
+check_git_changed_file() {
+	siteShouldRebuild=0
+	for dir in $(
+		echo $(
+			for path in $(git diff --name-only "${1}^..${2}")
+			do
+				echo $(dirname "$path")
+			done
+		) | xargs -n1 | sort -u
+	)
+	do
+		case $dir in
+			nginx)
+				rebuildNginx
+				;;
+			_posts)
+				updatePost
+				;;
+			*)
+				siteShouldRebuild=1
+				;;
+		esac
+	done
+	if [ $siteShouldRebuild = 1 ];
+	then
+		rebuildSite
+	fi
+}
+rebuildNginx() {
+	echo 'rebuild nginx'
+	sshRemote 'cd blog;sudo docker-compose build nginx;sudo docker-compose up -d'
+}
+
+rebuildSite() {
+	echo 'rebuild site'
+	sshRemote 'cd blog;sudo docker-compose build site;sudo docker-compose up -d'
+}
+
+updatePost() {
+	echo updatePost
+}
 
 sshRemote() {
 	ssh rlin@aboydfd.com "$1"
 }
 rebuild() {
-	sshRemote 'cd blog; git pull -r;sudo docker-compose build;sudo docker-compose up -d'
+	sshRemote 'cd blog;sudo docker-compose build;sudo docker-compose up -d'
 }
 update() {
-	echo 1
-	echo $GO_TO_REVISION_BLOG
-	echo $GO_FROM_REVISION_BLOG
-	echo $GO_FROM_REVISION_GOCD
-	echo $GO_TO_REVISION_GOCD
-	echo 2
 	sshRemote 'cd blog; git pull -r'
 }
 case $1 in
-	rebuild)
-		rebuild
+	bsite)
+		rebuildSite
+		;;
+	bnginx)
+		rebuildNginx
 		;;
 	update)
 		update
+		;;
+	*)
+		check_git_changed_file $GO_FROM_REVISION_BLOG $GO_TO_REVISION_BLOG
 		;;
 esac
